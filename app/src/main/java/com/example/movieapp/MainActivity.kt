5c9f45bef5ea78e0c5d345472c7912b7445375b8
Emmanuel Pius <emmCode4495@gmail.com>
2022-09-01 06:23:34 +0100

    "Created the basic structure of the movie app using Scaffold and LazyColumns"

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.movieapp.ui.theme.MovieAppTheme
@@ -14,7 +24,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
+            MyApp {
+                MainContent()
+            }
+
         }
     }
 }
@@ -34,19 +47,60 @@ fun MyApp(content: @Composable () ->Unit){
         }
     }
 
-@Composable
-fun MainContent(){
-    Surface(color = MaterialTheme.colors.background){
-        Text(text = "Hello")
-    }
-
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApp {
         MainContent()
+    }
+}
+
+
+@Composable
+fun MainContent(moveList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "HarryPotter",
+    "Superman",
+    "Spiderman 3",
+    "Justice League",
+    "Life",
+    "Star Wars",
+    "Hell Boy",
+    "Legend of the Seeker",
+    "Xena",
+    "The Minions 2"
+)){
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn{
+            items(items = moveList){
+                MovieRow(movie = it)
+
+            }
+        }
+
+    }
+}
+
+@Composable
+fun MovieRow(movie: String){
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp),
+    shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+    elevation = 6.dp){
+        Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start){
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+            shape = RectangleShape,
+            elevation = 4.dp) {
+                Icon(imageVector = Icons.Default.AccountBox,
+                    contentDescription = "default placeholder")
+            }
+            Text(text = movie)
+        }
     }
 }
