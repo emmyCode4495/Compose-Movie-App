2ce83fe22c530d43f783a967c2434d2f446a738c
Emmanuel Pius <emmCode4495@gmail.com>
2022-09-11 12:24:34 +0100

    Version of compose and kotlin changed

--- app/src/main/java/com/example/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/Widgets.kt
@@ -1,24 +1,28 @@
 package com.example.movieapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.movieapp.models.MovieProperties
+import com.example.movieapp.models.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: MovieProperties,
+fun MovieRow(movie: MovieProperties = getMovies()[0],
              onItemClick: (String) -> Unit = {}){
     Card(modifier = Modifier
         .padding(4.dp)
@@ -36,8 +40,19 @@ fun MovieRow(movie: MovieProperties,
                 .size(100.dp),
                 shape = RectangleShape,
                 elevation = 4.dp) {
-                Icon(imageVector = Icons.Default.AccountBox,
+//                    Image(painter = rememberImagePainter(data = movie.images[0]),
+//                    contentDescription = "Movie Poster")
+             Icon(imageVector = Icons.Default.AccountBox,
                     contentDescription = "default placeholder")
+            }
+
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = movie.title,
+                style = MaterialTheme.typography.h6)
+                Text(text = "Director: ${movie.title}",
+                style = MaterialTheme.typography.caption)
+                Text(text = "Released: ${movie.year}",
+                style = MaterialTheme.typography.caption)
             }
             Text(text = movie.title)
         }
